fb35c050ca4cea0ba725e70bbb8f4ddeef7e47bf
SeoU <tjdnfkd1225@naver.com>
2024-01-05 18:52:27 +0900

    Initial Commit

---
+++ TableViewControllerPractice/Case2TableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  Case2TableViewController.swift
+//  TableViewControllerPractice
+//
+//  Created by 은서우 on 2024/01/05.
+//
+
+import UIKit
+
+class Case2TableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    
+
+}
---
+++ TableViewControllerPractice/Case3TableViewController.swift
@@ -0,0 +1,19 @@
+//
+//  Case3TableViewController.swift
+//  TableViewControllerPractice
+//
+//  Created by 은서우 on 2024/01/05.
+//
+
+import UIKit
+
+class Case3TableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+   
+}
