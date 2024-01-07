aa4bbb7bb4de1deebdd8885bf8dec6193d91b203
SeoU <tjdnfkd1225@naver.com>
2024-01-08 04:44:34 +0900

    case 1~3 구현

---
+++ TableViewControllerPractice/ShoppingTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ShoppingTableViewCell.swift
+//  TableViewControllerPractice
+//
+//  Created by 은서우 on 2024/01/08.
+//
+
+import UIKit
+
+class ShoppingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
